15c99968be972889595fee9222eca83e263b3365
Ikvi10 <Ikviakmal10@gmail.com>
2023-06-14 15:09:22 +0700

    first commit

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dashboard form2 = new Dashboard();
+
+            // Menampilkan form lain
+            form2.Show();
+            //this.Close();
+        }
+    }
+}
